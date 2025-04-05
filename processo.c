875df93b07b0f36a509cd7243f86d087e160de81
David Moura <david.mourabr@gmail.com>
2025-04-05 16:16:14 -0300

    Ordenei os processos por id em ordem crescente, e por data juizamento e ordem decrescente.

--- processo.c
+++ processo.c
@@ -32,3 +32,42 @@ void ler_arquivo(Processo processos[], int *total) {
     fclose(arquivo);
     printf("Arquivo carregado com sucesso! Total de processos: %d\n", *total); //exibe o total de processos lidos
 }
+
+void ordenar_por_id(Processo processos[], int *total) {
+    for (int i = 0; i < total - 1; i++) { 
+        int minIndex = i;
+        
+        for (int j = i + 1; j < total; j++) { 
+            if (processos[j].id < processos[minIndex].id) {
+                minIndex = j;
+            }
+        }
+        
+        if (minIndex != i) {
+            Processo temp = processos[i];
+            processos[i] = processos[minIndex];
+            processos[minIndex] = temp;
+        }
+        printf("Processos ordenados por id (crescente).\n");
+    }
+}
+
+void data_juizamento(Processo processos[], int *total) {
+        for (int i = 0; i < total - 1; i++) {
+            int maxIndex = i;
+    
+            for (int j = i + 1; j < total; j++) {
+                if (strcmp(processos[j].data_ajuizamento, processos[maxIndex].data_ajuizamento) > 0) {
+                    maxIndex = j;
+                }
+            }
+    
+            if (maxIndex != i) {
+                Processo temp = processos[i];
+                processos[i] = processos[maxIndex];
+                processos[maxIndex] = temp;
+            }
+        }
+    
+        printf("Processos ordenados por data de ajuizamento (decrescente).\n");
+    }
